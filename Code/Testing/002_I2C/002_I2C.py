cbb93f38dbf0c1a197eca766786464b3571817c4
Mark <president.evans@gmail.com>
2016-11-30 12:46:37 +1100

    testing new servo wave pattern

--- Code/Testing/002_I2C/002_I2C.py
+++ Code/Testing/002_I2C/002_I2C.py
@@ -24,7 +24,7 @@ def main():
     dev.set_pwm_frequency(1600*0.9)   # Hz
     while True:
         for val in range(0,4096,8):
-            dev.set_pwm(5, (val + (4096/16)*5) % 4096)
+            dev.set_pwm(5, abs(4096-val*2))
             time.sleep(0.01)
     
 
