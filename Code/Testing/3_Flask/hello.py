f764943c6287c592ec89a00abb45bc2626b6858e
Mark <president.evans@gmail.com>
2016-12-02 15:01:57 +1100

    Webserver is working xo

--- Code/Testing/3_Flask/hello.py
+++ Code/Testing/3_Flask/hello.py
@@ -5,12 +5,12 @@ from collections import namedtuple
 
 NUM_SERVOS = 16
 
-servoDb = [ {'index':index,'limb':'','name':'','pos':90,'max':180} for index in range(NUM_SERVOS) ]
+servoDb = [ {'index':index,'limb':'','name':'','pos':90,'min':0,'max':180} for index in range(NUM_SERVOS+1) ]
 servoDb[ 1]['limb'] = 'Neck'
 servoDb[ 1]['name'] = 'Spin'
 servoDb[ 2]['limb'] = 'Neck'
 servoDb[ 2]['name'] = 'Nod'
-servoDb[ 3]['limb'] = 'Larm'
+servoDb[ 3]['limb'] = 'LArm'
 servoDb[ 3]['name'] = 'Shoulder rotate'
 servoDb[ 4]['limb'] = 'LArm'
 servoDb[ 4]['name'] = 'Shoulder up/down'
@@ -27,18 +27,33 @@ servoDb[ 9]['name'] = 'Elbow'
 servoDb[10]['limb'] = 'RArm'
 servoDb[10]['name'] = 'Grip'
 
+servoDb[11]['limb'] = 'LLeg'
+servoDb[11]['name'] = 'Hip'
+servoDb[12]['limb'] = 'LLeg'
+servoDb[12]['name'] = 'Knee'
+servoDb[13]['limb'] = 'LLeg'
+servoDb[13]['name'] = 'Ankle'
+
+servoDb[14]['limb'] = 'RLeg'
+servoDb[14]['name'] = 'Hip'
+servoDb[15]['limb'] = 'RLeg'
+servoDb[15]['name'] = 'Knee'
+servoDb[16]['limb'] = 'RLeg'
+servoDb[16]['name'] = 'Ankle'
+
 limbs = []
-for i in range(NUM_SERVOS):
+for i in range(1,NUM_SERVOS+1):
     if not servoDb[i]['limb'] in limbs:
-        limbs.append(servoDb[i]['limb'])
+        if not servoDb[i]['limb'] == '':
+            limbs.append(servoDb[i]['limb'])
 
 
 app = Flask(__name__)
 
 def update_servos():
-    if(request.form['Servo'] == 'Neck'):
-        pwm_servo(1, request.form['Servo1'])
-        pwm_servo(2, request.form['Servo2'])
+    for i in range (1,NUM_SERVOS+1):
+        servoDb[i]['pos'] = request.form['Servo' + str(i)]
+        print( "Servo" + str(i) + ": " + str(servoDb[i]['pos']) )
 
 
 @app.route("/",methods=['GET','POST','PUT'])
@@ -49,4 +64,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=1)
