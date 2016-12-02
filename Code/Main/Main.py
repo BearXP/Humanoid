cb45555822db7bd03c63a1ad0c4aff0e7909a855
Mark <president.evans@gmail.com>
2016-12-02 15:35:00 +1100

    testing pwm_val

--- Code/Main/Main.py
+++ Code/Main/Main.py
@@ -21,43 +21,41 @@ import time
 NUM_SERVOS = 16
 
 servoDb = [ {'index':index,'limb':'','name':'','pos':90,'min':0,'max':180} for index in range(NUM_SERVOS+1) ]
+servoDb[ 0]['limb'] = 'Neck'
+servoDb[ 0]['name'] = 'Spin'
 servoDb[ 1]['limb'] = 'Neck'
-servoDb[ 1]['name'] = 'Spin'
-servoDb[ 2]['limb'] = 'Neck'
-servoDb[ 2]['name'] = 'Nod'
+servoDb[ 1]['name'] = 'Nod'
+servoDb[ 2]['limb'] = 'LArm'
+servoDb[ 2]['name'] = 'Shoulder rotate'
 servoDb[ 3]['limb'] = 'LArm'
-servoDb[ 3]['name'] = 'Shoulder rotate'
+servoDb[ 3]['name'] = 'Shoulder up/down'
 servoDb[ 4]['limb'] = 'LArm'
-servoDb[ 4]['name'] = 'Shoulder up/down'
+servoDb[ 4]['name'] = 'Elbow'
 servoDb[ 5]['limb'] = 'LArm'
-servoDb[ 5]['name'] = 'Elbow'
-servoDb[ 6]['limb'] = 'LArm'
-servoDb[ 6]['name'] = 'Grip'
+servoDb[ 5]['name'] = 'Grip'
+servoDb[ 6]['limb'] = 'RArm'
+servoDb[ 6]['name'] = 'Shoulder rotate'
 servoDb[ 7]['limb'] = 'RArm'
-servoDb[ 7]['name'] = 'Shoulder rotate'
+servoDb[ 7]['name'] = 'Shoulder up/down'
 servoDb[ 8]['limb'] = 'RArm'
-servoDb[ 8]['name'] = 'Shoulder up/down'
+servoDb[ 8]['name'] = 'Elbow'
 servoDb[ 9]['limb'] = 'RArm'
-servoDb[ 9]['name'] = 'Elbow'
-servoDb[10]['limb'] = 'RArm'
-servoDb[10]['name'] = 'Grip'
-
+servoDb[ 9]['name'] = 'Grip'
+servoDb[10]['limb'] = 'LLeg'
+servoDb[10]['name'] = 'Hip'
 servoDb[11]['limb'] = 'LLeg'
-servoDb[11]['name'] = 'Hip'
+servoDb[11]['name'] = 'Knee'
 servoDb[12]['limb'] = 'LLeg'
-servoDb[12]['name'] = 'Knee'
-servoDb[13]['limb'] = 'LLeg'
-servoDb[13]['name'] = 'Ankle'
-
+servoDb[12]['name'] = 'Ankle'
+servoDb[13]['limb'] = 'RLeg'
+servoDb[13]['name'] = 'Hip'
 servoDb[14]['limb'] = 'RLeg'
-servoDb[14]['name'] = 'Hip'
+servoDb[14]['name'] = 'Knee'
 servoDb[15]['limb'] = 'RLeg'
-servoDb[15]['name'] = 'Knee'
-servoDb[16]['limb'] = 'RLeg'
-servoDb[16]['name'] = 'Ankle'
+servoDb[15]['name'] = 'Ankle'
 
 limbs = []
-for i in range(1,NUM_SERVOS+1):
+for i in range(NUM_SERVOS):
     if not servoDb[i]['limb'] in limbs:
         if not servoDb[i]['limb'] == '':
             limbs.append(servoDb[i]['limb'])
@@ -68,10 +66,12 @@ ServoController.set_pwm_frequency(60)
 app = Flask(__name__)
 
 def update_servos():
-    for i in range (1,NUM_SERVOS+1):
+    for i in range (NUM_SERVOS):
         servoDb[i]['pos'] = request.form['Servo' + str(i)]
         pwm_val = 150.0 + float(servoDb[i]['pos']) * 65.0 / 18.0
+        print("Servo"+i+": "+servoDb[i]['pos']+" > "+pwm_val)
         ServoController.set_pwm(i, int(pwm_val))
+        time.sleep(0.01)
 
 
 @app.route("/",methods=['GET','POST','PUT'])
