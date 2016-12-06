a14ce27c73ee5965bc79bdb2fd2786bdaa322629
Mark <president.evans@gmail.com>
2016-12-06 22:09:35 +1100

    Started making setup.sh for getting the code to work

---
+++ Code/setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -f build-essential python-pip python-dev python-smbus 
+git
+sudo pip install Flask
+git clone https://github.com/xtacocorex/Adafruit_Python_GPIO.git
+cd Adafruit_Python_GPIO
+sudo python setup.py install
